d5bc3037a1b3d9030211ec218f1984a85ef86caf
Christian Glodt <chris@mind.lu>
2024-01-08 20:20:42 +0000

    Add roboto font

--- plngx-dissect-frontend/src/main.tsx
+++ plngx-dissect-frontend/src/main.tsx
@@ -6,6 +6,12 @@ import './index.css'
 import ErrorPage from './ErrorPage.tsx'
 import PatternEditor from './PatternEditor.tsx'
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 const router = createBrowserRouter([
   {
     path: "/",
