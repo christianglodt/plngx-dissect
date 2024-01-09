65507176169a7daa81ffb187e1c994cb7af30415
Christian Glodt <chris@mind.lu>
2024-01-09 16:33:17 +0000

    move functions into class so that an abstract base class can be made

--- backend/cache.py
+++ backend/cache.py
@@ -14,41 +14,9 @@ NoValueType = NewType('NoValue', object)
 NO_VALUE: NoValueType = NoValueType(object())
 
 
-def cache_key(args: tuple[Any, ...], kwargs: dict[str, Any]) -> str:
-    key_str = json.dumps({ "args": args, "kwargs": kwargs })
-    hash = hashlib.sha256()
-    hash.update(key_str.encode('utf-8'))
-    return hash.hexdigest()
-
-
-def _get_cache_dir(cache_name: str) -> pathlib.Path:
-    cache_dir = pathlib.Path(f'/cache') / cache_name
-    cache_dir.mkdir(parents=True, exist_ok=True)
-    return cache_dir
-
-
 @lru_cache(maxsize=128)
 def parse_yaml(s: str) -> Any:
     return ruamel.yaml.YAML().load(StringIO(s))
-
-
-async def get_cache_value(cache_name: str, key: str, Model: Type[pydantic.BaseModel]) -> pydantic.BaseModel | NoValueType:
-    try:
-        async with aiofiles.open(_get_cache_dir(cache_name) / key, mode='r', encoding='utf-8') as f:
-            contents = await f.read()
-            obj = parse_yaml(contents)
-            return Model.model_validate(obj)
-    except FileNotFoundError:
-        return NO_VALUE
-
-
-async def set_cache_value(cache_name: str, key: str, obj: pydantic.BaseModel):
-    async with aiofiles.tempfile.NamedTemporaryFile('w', encoding='utf-8', prefix=f'f{cache_name}-{key}', dir=_get_cache_dir(cache_name)) as f:
-        stream = StringIO()
-        ruamel.yaml.YAML().dump(obj.model_dump(), stream)
-        await f.write(stream.getvalue())
-        # atomically rename-into-place
-        await aiofiles.os.rename(str(f.name), _get_cache_dir(cache_name) / key)
 
 
 P = ParamSpec('P')
@@ -57,17 +25,42 @@ class pydantic_yaml_cache:
     def __init__(self, Model: Type[pydantic.BaseModel], cache_name: str):
         self.Model = Model
         self.cache_name = cache_name
+        self.cache_dir = pathlib.Path(f'/cache') / cache_name
+        self.cache_dir.mkdir(parents=True, exist_ok=True)
 
     def __call__(self, f: Callable[P, Awaitable[pydantic.BaseModel]]) -> Callable[P, Awaitable[pydantic.BaseModel]]:
         @wraps(f)
         async def wrapper(*args: P.args, **kwargs: P.kwargs) -> pydantic.BaseModel:
-            key = cache_key(args, kwargs)
-            cached_value = await get_cache_value(self.cache_name, key, self.Model)
+            key = self.cache_key(args, kwargs)
+            cached_value = await self.get_cache_value(key, self.Model)
             if cached_value != NO_VALUE:
                 return cast(pydantic.BaseModel, cached_value)
             
             value = await f(*args, **kwargs)
-            await set_cache_value(self.cache_name, key, value)
+            await self.set_cache_value(key, value)
 
             return value
         return wrapper
+
+    def cache_key(self, args: tuple[Any, ...], kwargs: dict[str, Any]) -> str:
+        key_str = json.dumps({ "args": args, "kwargs": kwargs })
+        hash = hashlib.sha256()
+        hash.update(key_str.encode('utf-8'))
+        return hash.hexdigest()
+
+    async def get_cache_value(self, key: str, Model: Type[pydantic.BaseModel]) -> pydantic.BaseModel | NoValueType:
+        try:
+            async with aiofiles.open(self.cache_dir / key, mode='r', encoding='utf-8') as f:
+                contents = await f.read()
+                obj = parse_yaml(contents)
+                return Model.model_validate(obj)
+        except FileNotFoundError:
+            return NO_VALUE
+
+    async def set_cache_value(self, key: str, obj: pydantic.BaseModel):
+        async with aiofiles.tempfile.NamedTemporaryFile('w', encoding='utf-8', prefix=f'f{self.cache_name}-{key}', dir=self.cache_dir) as f:
+            stream = StringIO()
+            ruamel.yaml.YAML().dump(obj.model_dump(), stream)
+            await f.write(stream.getvalue())
+            # atomically rename-into-place
+            await aiofiles.os.rename(str(f.name), self.cache_dir / key)
