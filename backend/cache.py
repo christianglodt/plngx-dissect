e609bf9ec4310259a1cb125731eb7c14b91b16a5
Christian Glodt <chris@mind.lu>
2024-01-09 19:53:42 +0000

    make abstract base class for cache decorator

--- backend/cache.py
+++ backend/cache.py
@@ -1,13 +1,14 @@
 import pathlib
 import pydantic
 import ruamel.yaml
-from typing import Type, Any, NewType, Callable, ParamSpec, Awaitable, cast
+from typing import Type, Any, NewType, Callable, ParamSpec, Awaitable, cast, TypeVar
 from ruamel.yaml.compat import StringIO
 from functools import wraps, lru_cache
 import json
 import hashlib
 import aiofiles
 import aiofiles.os
+import abc
 
 
 NoValueType = NewType('NoValue', object)
@@ -20,21 +21,22 @@ def parse_yaml(s: str) -> Any:
 
 
 P = ParamSpec('P')
+T = TypeVar('T')
 
-class pydantic_yaml_cache:
-    def __init__(self, Model: Type[pydantic.BaseModel], cache_name: str):
-        self.Model = Model
+
+class AsyncBaseCache[T](abc.ABC):
+    def __init__(self, cache_name: str):
         self.cache_name = cache_name
         self.cache_dir = pathlib.Path(f'/cache') / cache_name
         self.cache_dir.mkdir(parents=True, exist_ok=True)
 
-    def __call__(self, f: Callable[P, Awaitable[pydantic.BaseModel]]) -> Callable[P, Awaitable[pydantic.BaseModel]]:
+    def __call__(self, f: Callable[P, Awaitable[T]]) -> Callable[P, Awaitable[T]]:
         @wraps(f)
-        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> pydantic.BaseModel:
+        async def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
             key = self.cache_key(args, kwargs)
-            cached_value = await self.get_cache_value(key, self.Model)
+            cached_value = await self.get_cache_value(key)
             if cached_value != NO_VALUE:
-                return cast(pydantic.BaseModel, cached_value)
+                return cast(T, cached_value)
             
             value = await f(*args, **kwargs)
             await self.set_cache_value(key, value)
@@ -48,19 +50,43 @@ class pydantic_yaml_cache:
         hash.update(key_str.encode('utf-8'))
         return hash.hexdigest()
 
-    async def get_cache_value(self, key: str, Model: Type[pydantic.BaseModel]) -> pydantic.BaseModel | NoValueType:
+    @abc.abstractmethod
+    def load(self, data: bytes) -> T:
+        raise NotImplementedError
+    
+    @abc.abstractmethod
+    def dump(self, value: T) -> bytes:
+        raise NotImplementedError
+
+
+    async def get_cache_value(self, key: str) -> T | NoValueType:
         try:
-            async with aiofiles.open(self.cache_dir / key, mode='r', encoding='utf-8') as f:
-                contents = await f.read()
-                obj = parse_yaml(contents)
-                return Model.model_validate(obj)
+            async with aiofiles.open(self.cache_dir / key, mode='rb') as f:
+                return self.load(await f.read())
         except FileNotFoundError:
             return NO_VALUE
 
-    async def set_cache_value(self, key: str, obj: pydantic.BaseModel):
-        async with aiofiles.tempfile.NamedTemporaryFile('w', encoding='utf-8', prefix=f'f{self.cache_name}-{key}', dir=self.cache_dir) as f:
-            stream = StringIO()
-            ruamel.yaml.YAML().dump(obj.model_dump(), stream)
-            await f.write(stream.getvalue())
+    async def set_cache_value(self, key: str, obj: T):
+        async with aiofiles.tempfile.NamedTemporaryFile('wb', prefix=f'f{self.cache_name}-{key}', dir=self.cache_dir) as f:
+            await f.write(self.dump(obj))
             # atomically rename-into-place
             await aiofiles.os.rename(str(f.name), self.cache_dir / key)
+
+
+PT = TypeVar('PT', bound=pydantic.BaseModel)
+
+
+class pydantic_yaml_cache(AsyncBaseCache[PT]):
+
+    def __init__(self, Model: Type[PT], cache_name: str):
+        super().__init__(cache_name)
+        self.Model = Model
+
+    def load(self, data: bytes) -> PT:
+        obj = parse_yaml(data.decode('utf-8'))
+        return self.Model.model_validate(obj)
+
+    def dump(self, value: PT) -> bytes:
+        stream = StringIO()
+        ruamel.yaml.YAML().dump(value.model_dump(), stream)
+        return stream.getvalue().encode('utf-8')
