39f414836baaa0e851b1b2cfd3295aaad50584df
Christian Glodt <chris@mind.lu>
2024-01-11 10:01:39 +0000

    format to 4 spaces

--- plngx-dissect-frontend/src/ErrorPage.tsx
+++ plngx-dissect-frontend/src/ErrorPage.tsx
@@ -2,30 +2,30 @@ import { Alert, AlertTitle, Box } from "@mui/material";
 import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 function errorMessage(error: unknown): string {
-  if (isRouteErrorResponse(error)) {
-    return `${error.status} ${error.statusText}`
-  } else if (error instanceof Error) {
-    return error.message
-  } else if (typeof error === 'string') {
-    return error
-  } else {
-    console.error(error)
-    return 'Unknown error'
-  }
+    if (isRouteErrorResponse(error)) {
+        return `${error.status} ${error.statusText}`
+    } else if (error instanceof Error) {
+        return error.message
+    } else if (typeof error === 'string') {
+        return error
+    } else {
+        console.error(error)
+        return 'Unknown error'
+    }
 }
 
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+    const error = useRouteError();
+    console.error(error);
 
-  return (
-    <Box sx={{ display: 'flex', width: '100%', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
-      <Alert severity="error">
-        <AlertTitle>
-          Error
-        </AlertTitle>
-        {errorMessage(error)}
-      </Alert>
-    </Box>
-  );
+    return (
+        <Box sx={{ display: 'flex', width: '100%', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+            <Alert severity="error">
+                <AlertTitle>
+                    Error
+                </AlertTitle>
+                {errorMessage(error)}
+            </Alert>
+        </Box>
+    );
 }
