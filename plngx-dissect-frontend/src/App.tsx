e78402345a483cf8d31a1b95a1f316f0dad7100b
Christian Glodt <chris@mind.lu>
2024-01-11 10:01:30 +0000

    Add padding around content, add links and set dynamic title

--- plngx-dissect-frontend/src/App.tsx
+++ plngx-dissect-frontend/src/App.tsx
@@ -5,27 +5,32 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
-import { Outlet, useParams } from 'react-router-dom';
+import { Link, Outlet, useParams } from 'react-router-dom';
 
 export default function App() {
 
-  const params = useParams();
-  
-  const icon = params.patternId ? <ChevronLeftIcon/> : <MenuIcon/>
+    const params = useParams();
 
-  return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-          <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            { icon }
-          </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            Patterns
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      <Outlet/>
-    </Box>
-  );
+    const icon = params.patternId ? <ChevronLeftIcon /> : <MenuIcon />
+    const title = params.patternId ? `Pattern “${params.patternId}”` : 'Patterns';
+
+    return (
+        <Box sx={{ maxHeight: '100%', height: '100%', display: 'flex', flexDirection: 'column' }}>
+            <AppBar position="relative">
+                <Toolbar>
+                    <Link to={'/'}>
+                        <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+                            {icon}
+                        </IconButton>
+                    </Link>
+                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                        {title}
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Box sx={{ padding: '1rem', height: '100%' }}>
+                <Outlet />
+            </Box>
+        </Box>
+    );
 }
