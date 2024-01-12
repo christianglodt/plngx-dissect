8662451e5e36febe83816a8fe9af5656f44bd238
Christian Glodt <chris@mind.lu>
2024-01-12 21:09:17 +0000

    make CheckItemDialog generic

--- plngx-dissect-frontend/src/checks/NumPagesCheckItem.tsx
+++ plngx-dissect-frontend/src/checks/NumPagesCheckItem.tsx
@@ -1,36 +1,25 @@
 import { Input, ListItemText } from "@mui/material";
 import { NumPagesCheck } from "../types";
 import React, { useState } from "react";
-import { produce } from "immer";
 import CheckItemDialog from "../utils/CheckItemDialog";
 import CheckItem, { CheckItemDialogPropsType } from "../utils/CheckItem";
 
 
 const NumPagesCheckDialog = (props: CheckItemDialogPropsType<NumPagesCheck>) => {
-    const { check, open, onClose, onChange, onDelete } = props;
 
-    const [value, setValue] = useState(check.num_pages);
+    const [value, setValue] = useState(props.check.num_pages);
 
     const onNumberChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
         const n = Number(event.target.value);
         setValue(n > 0 ? n : 1)
     }
 
-    const onConfirmed = () => {
-        const newCheck = produce(check, draft => {
-            draft.num_pages = value;
-        });
-        onChange(newCheck);
-        onClose();
-    }
-
-    const onDeleteConfirmed = () => {
-        onDelete();
-        onClose();
+    const onChangeDraft = (draft: NumPagesCheck) => {
+        draft.num_pages = value;
     }
 
     return (
-        <CheckItemDialog title="Check Number of Pages" open={open} onClose={onClose} onConfirmed={onConfirmed} onDelete={onDeleteConfirmed}>
+        <CheckItemDialog<NumPagesCheck> title="Check Number of Pages" onChangeDraft={onChangeDraft} {...props}>
             <div>Number of pages must be:</div>
             <Input type="number" value={value} onChange={onNumberChanged}></Input>
         </CheckItemDialog>
--- plngx-dissect-frontend/src/utils/CheckItemDialog.tsx
+++ plngx-dissect-frontend/src/utils/CheckItemDialog.tsx
@@ -1,18 +1,34 @@
 import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from "@mui/material";
 import React from "react";
 import ConfirmButton from "./ConfirmButton";
+import { Draft, produce } from "immer";
 
-type CheckItemDialogPropsType = {
+type CheckItemDialogPropsType<CheckType> = {
     title: string | React.JSX.Element;
+    check: CheckType;
     open: boolean;
-    onConfirmed: () => void;
+    onChange: (newCheck: CheckType) => void;
+    onChangeDraft: (draft: Draft<CheckType>) => void;
     onClose: () => void;
     onDelete: () => void;
-    children?: string | React.JSX.Element[];
+    children?: React.ReactNode[] | React.ReactNode;
 }
 
-const CheckItemDialog = (props: CheckItemDialogPropsType) => {
-    const { title, open, onConfirmed, onClose, onDelete } = props;
+const CheckItemDialog = <CheckType,>(props: CheckItemDialogPropsType<CheckType>) => {
+    const { title, check, open, onChange, onChangeDraft, onClose, onDelete } = props;
+
+    const onConfirmed = () => {
+        const newCheck = produce(check, draft => {
+            onChangeDraft(draft);
+        });
+        onChange(newCheck);
+        onClose();
+    }
+
+    const onDeleteConfirmed = () => {
+        onDelete();
+        onClose();
+    };
 
     return (
         <Dialog open={open} onClose={onClose}>
@@ -21,7 +37,7 @@ const CheckItemDialog = (props: CheckItemDialogPropsType) => {
                 {props.children}
             </DialogContent>
             <DialogActions>
-                <ConfirmButton variant="text" color="warning" sx={{ marginRight: 'auto' }} dialogTitle="Delete Check?" dialogText="Are you sure to delete this check?" onConfirmed={onDelete}>Delete</ConfirmButton>
+                <ConfirmButton variant="text" color="warning" sx={{ marginRight: 'auto' }} dialogTitle="Delete Check?" dialogText="Are you sure to delete this check?" onConfirmed={onDeleteConfirmed}>Delete</ConfirmButton>
                 <Button variant="outlined" onClick={onClose}>Cancel</Button>
                 <Button variant="contained" onClick={onConfirmed}>Ok</Button>
             </DialogActions>
