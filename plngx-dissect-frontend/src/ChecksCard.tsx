ca3990fff55b3d8a2ee725d85491a04f3e5c3185
Christian Glodt <chris@mind.lu>
2024-01-15 09:08:58 +0000

    refactor CheckItemFactory into its own file

--- plngx-dissect-frontend/src/ChecksCard.tsx
+++ plngx-dissect-frontend/src/ChecksCard.tsx
@@ -1,40 +1,9 @@
-import React from "react";
 import { produce } from "immer";
 
-import { Pattern, Check, CheckTypeId, NumPagesCheck, RegionRegexCheck, TitleRegexCheck, CorrespondentCheck, StoragePathCheck, DocumentTypeCheck, TagCheck, DateCreatedCheck } from "./types";
-import NumPagesCheckItem from "./checks/NumPagesCheckItem";
+import { Pattern, Check } from "./types";
 import ListCard from "./utils/ListCard";
 import CreateCheckItemButton from "./checks/CreateCheckItemButton";
-import RegionRegexCheckItem from "./checks/RegionRegexCheckItem";
-import TitleRegexCheckItem from "./checks/TitleRegexCheckItem";
-import { CheckItemPropsType } from "./utils/CheckItem";
-import CorrespondentCheckItem from "./checks/CorrespondentCheckItem";
-import DocumentTypeCheckItem from "./checks/DocumentTypeCheckItem";
-import StoragePathCheckItem from "./checks/StoragePathCheckItem";
-import TagCheckItem from "./checks/TagCheckItem";
-import DateCreatedCheckItem from "./checks/DateCreatedCheckItem";
-
-
-const CheckItemFactory = (props: CheckItemPropsType<Check>): React.JSX.Element => {
-    switch (props.check.type) {
-        case CheckTypeId.NumPages:
-            return <NumPagesCheckItem      {...props as CheckItemPropsType<NumPagesCheck>}/>;
-        case CheckTypeId.Region:
-            return <RegionRegexCheckItem   {...props as CheckItemPropsType<RegionRegexCheck>}/>;
-        case CheckTypeId.Title:
-            return <TitleRegexCheckItem    {...props as CheckItemPropsType<TitleRegexCheck>}/>;
-        case CheckTypeId.Correspondent:
-            return <CorrespondentCheckItem {...props as CheckItemPropsType<CorrespondentCheck>}/>;
-        case CheckTypeId.DocumentType:
-            return <DocumentTypeCheckItem  {...props as CheckItemPropsType<DocumentTypeCheck>}/>;
-        case CheckTypeId.StoragePath:
-            return <StoragePathCheckItem   {...props as CheckItemPropsType<StoragePathCheck>}/>;
-        case CheckTypeId.Tag:
-            return <TagCheckItem           {...props as CheckItemPropsType<TagCheck>}/>;
-        case CheckTypeId.DateCreated:
-            return <DateCreatedCheckItem   {...props as CheckItemPropsType<DateCreatedCheck>}/>;
-    }
-};
+import CheckItemFactory from "./checks/CheckItemFactory";
 
 type ChecksCardProps = {
     pattern: Pattern;
@@ -69,7 +38,7 @@ const ChecksCard = (props: ChecksCardProps) => {
     return (
         <ListCard title="Checks" headerWidget={<CreateCheckItemButton onCheckCreated={onCheckCreated}/>}>
             { pattern.checks.map((check, index) =>
-                <CheckItemFactory key={index} check={check} onChange={(newCheck) => onCheckChange(index, newCheck)} onDelete={() => onCheckDelete(index)}/>
+                <CheckItemFactory key={index} check={check} onChange={(newCheck: Check) => onCheckChange(index, newCheck)} onDelete={() => onCheckDelete(index)}/>
             )}
         </ListCard>
     );
---
+++ plngx-dissect-frontend/src/checks/CheckItemFactory.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+import { Check, CheckTypeId, NumPagesCheck, RegionRegexCheck, TitleRegexCheck, CorrespondentCheck, StoragePathCheck, DocumentTypeCheck, TagCheck, DateCreatedCheck } from "../types";
+import NumPagesCheckItem from "./NumPagesCheckItem";
+import RegionRegexCheckItem from "./RegionRegexCheckItem";
+import TitleRegexCheckItem from "./TitleRegexCheckItem";
+import { CheckItemPropsType } from "../utils/CheckItem";
+import CorrespondentCheckItem from "./CorrespondentCheckItem";
+import DocumentTypeCheckItem from "./DocumentTypeCheckItem";
+import StoragePathCheckItem from "./StoragePathCheckItem";
+import TagCheckItem from "./TagCheckItem";
+import DateCreatedCheckItem from "./DateCreatedCheckItem";
+
+const CheckItemFactory = (props: CheckItemPropsType<Check>): React.JSX.Element => {
+    switch (props.check.type) {
+        case CheckTypeId.NumPages:
+            return <NumPagesCheckItem      {...props as CheckItemPropsType<NumPagesCheck>}/>;
+        case CheckTypeId.Region:
+            return <RegionRegexCheckItem   {...props as CheckItemPropsType<RegionRegexCheck>}/>;
+        case CheckTypeId.Title:
+            return <TitleRegexCheckItem    {...props as CheckItemPropsType<TitleRegexCheck>}/>;
+        case CheckTypeId.Correspondent:
+            return <CorrespondentCheckItem {...props as CheckItemPropsType<CorrespondentCheck>}/>;
+        case CheckTypeId.DocumentType:
+            return <DocumentTypeCheckItem  {...props as CheckItemPropsType<DocumentTypeCheck>}/>;
+        case CheckTypeId.StoragePath:
+            return <StoragePathCheckItem   {...props as CheckItemPropsType<StoragePathCheck>}/>;
+        case CheckTypeId.Tag:
+            return <TagCheckItem           {...props as CheckItemPropsType<TagCheck>}/>;
+        case CheckTypeId.DateCreated:
+            return <DateCreatedCheckItem   {...props as CheckItemPropsType<DateCreatedCheck>}/>;
+    }
+};
+
+export default CheckItemFactory;
