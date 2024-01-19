e6487cb48a8361ee18cd92845d4d000a97e82959
Christian Glodt <chris@mind.lu>
2024-01-19 14:13:31 +0000

    refactor out generic input dialog button component

---
+++ plngx-dissect-frontend/src/utils/InputDialogButton.tsx
@@ -0,0 +1,46 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, TextField } from '@mui/material';
+import { useState } from 'react';
+
+
+type InputDialogPropsType = {
+    dialogTitle: string;
+    dialogText: string;
+    label: string;
+    onConfirmed: (value: string) => void;
+}
+
+const InputDialogButton = (props: InputDialogPropsType) => {
+
+    const [dialogOpen, setDialogOpen] = useState(false);
+    const [value, setValue] = useState('');
+
+    const onDialogClosed = () => {
+        setDialogOpen(false);
+        setValue('');
+    }
+
+    const onConfirmed = () => {
+        props.onConfirmed(value);
+        onDialogClosed();
+    }
+
+    const errorMessage = value.trim() === '' ? 'Can not be empty' : null;
+
+    return (
+        <>
+            <Dialog open={dialogOpen} onClose={onDialogClosed}>
+                <DialogTitle>{props.dialogTitle}</DialogTitle>
+                <DialogContent>
+                    <TextField sx={{ marginTop: '0.5rem' }} autoFocus required label="New Pattern Name" fullWidth value={value} onChange={(event) => setValue(event.target.value)} error={errorMessage !== null} helperText={errorMessage}/>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={onDialogClosed}>Cancel</Button>
+                    <Button onClick={onConfirmed}>{props.label}</Button>
+                </DialogActions>
+            </Dialog>
+            <Button onClick={() => setDialogOpen(true)}>{props.label}</Button>
+        </>
+    );
+};
+
+export default InputDialogButton;
