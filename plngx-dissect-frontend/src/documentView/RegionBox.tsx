47778e280616bfbd17525a83b6de4a2f085f1b42
Christian Glodt <chris@mind.lu>
2024-01-25 19:55:22 +0000

    implement drag and drop differently, fix resizing

--- plngx-dissect-frontend/src/documentView/RegionBox.tsx
+++ plngx-dissect-frontend/src/documentView/RegionBox.tsx
@@ -1,7 +1,8 @@
-import { DragEvent, useState } from 'react';
+import { useState } from 'react';
 import { Region } from '../types';
 import './RegionBox.css';
 import { produce } from 'immer';
+import { useDrag } from '@use-gesture/react';
 
 type RegionBoxPropsType<RegionType> = {
     region: RegionType;
@@ -19,72 +20,64 @@ const RegionBox = <RegionType extends Region,>(props: RegionBoxPropsType<RegionT
     const pxToPtH = (px: number) => px / props.pageClientRectDimensions[0] * props.pageWidth;
     const pxToPtV = (px: number) => px / props.pageClientRectDimensions[1] * props.pageHeight;
 
-    const [widthDuringResize, setWidthDuringResize] = useState<number|null>(null);
-    const [heightDuringResize, setHeightDuringResize] = useState<number|null>(null);
-    const initialWidth = props.region.x2 - props.region.x;
-    const initialHeight = props.region.y2 - props.region.y;
-    const width = widthDuringResize !== null ? widthDuringResize : initialWidth;
-    const height = heightDuringResize !== null ? heightDuringResize : initialHeight;
+    const [moveX, setMoveX] = useState(0);
+    const [moveY, setMoveY] = useState(0);
+    const [resizeX, setResizeX] = useState(0);
+    const [resizeY, setResizeY] = useState(0);
+
+    const width = props.region.x2 - props.region.x;
+    const height = props.region.y2 - props.region.y;
 
     const style: React.CSSProperties = {
-        top: `${ptToPercentV(props.region.y)}%`,
         left: `${ptToPercentH(props.region.x)}%`,
-        width: `${ptToPercentH(width)}%`,
-        height: `${ptToPercentV(height)}%`,
+        top: `${ptToPercentV(props.region.y)}%`,
+        width: `calc(${ptToPercentH(width)}% + ${resizeX}px)`,
+        height: `calc(${ptToPercentV(height)}% + ${resizeY}px)`,
+        transform: `translate(${moveX}px, ${moveY}px)`,
+        touchAction: 'none'
     };
 
-    const [dragStart, setDragStart] = useState<Array<number>>([0, 0]);
+    const dragBind = useDrag((state) => {
+        if (state.type == 'pointermove') {
+            setMoveX(state.movement[0]);
+            setMoveY(state.movement[1]);
+        }
+        if (state.type == 'pointerup') {
+            const mx = moveX;
+            const my = moveY;
+            setMoveX(0);
+            setMoveY(0);
+            props.onChange(produce(props.region, draft => {
+                draft.x = Math.round(draft.x + pxToPtH(mx));
+                draft.y = Math.round(draft.y + pxToPtV(my));
+                draft.x2 = Math.round(draft.x2 + pxToPtH(mx));
+                draft.y2 = Math.round(draft.y2 + pxToPtV(my));
+            }));
+        }
+    }, { preventDefault: true });
 
-    const onMoveDragStart = (event: DragEvent<HTMLDivElement>) => {
-        setDragStart([event.pageX, event.pageY]);
-        event.dataTransfer.setData('text/plain', 'dummy data'); // required to prevent "drag aborted" animation
-
-    };
-
-    const onMoveDragEnd = (event: DragEvent<HTMLDivElement>) => {
-        const dragXPx = (event.pageX - dragStart[0]);
-        const dragYPx = (event.pageY - dragStart[1]);
-        const dragXPt = pxToPtH(dragXPx);
-        const dragYPt = pxToPtV(dragYPx);
-
-        props.onChange(produce(props.region, draft => {
-            draft.x = Math.round(draft.x + dragXPt);
-            draft.y = Math.round(draft.y + dragYPt);
-            draft.x2 = Math.round(draft.x2 + dragXPt);
-            draft.y2 = Math.round(draft.y2 + dragYPt);
-        }));
-    };
-
-    const onResizeDragStart = (event: DragEvent<HTMLDivElement>) => {
-        setDragStart([event.pageX, event.pageY]);
-        event.dataTransfer.setData('text/plain', 'dummy data'); // required to prevent "drag aborted" animation
-        event.stopPropagation();
-    };
-
-    const onResizeDragEnd = (event: DragEvent<HTMLDivElement>) => {
-        event.stopPropagation();
-        const dX = pxToPtH(event.pageX - dragStart[0]);
-        const dY = pxToPtV(event.pageY - dragStart[1]);
-        props.onChange(produce(props.region, draft => {
-            draft.x2 = Math.round(draft.x2 + dX);
-            draft.y2 = Math.round(draft.y2 + dY);
-        }));
-        setWidthDuringResize(null);
-        setHeightDuringResize(null);
-    };
-    
-    const onResizeDrag = (event: DragEvent<HTMLDivElement>) => {
-        event.stopPropagation();
-        const dX = pxToPtH(event.pageX - dragStart[0]);
-        const dY = pxToPtV(event.pageY - dragStart[1]);
-        setWidthDuringResize(initialWidth + dX);
-        setHeightDuringResize(initialHeight + dY);
-    };
+    const resizeBind = useDrag((state) => {
+        state.event.stopPropagation(); // Necessary to ensure move handler of parent element is not invoked
+        if (state.type == 'pointermove') {
+            setResizeX(state.movement[0]);
+            setResizeY(state.movement[1]);
+        }
+        if (state.type == 'pointerup') {
+            const rx = resizeX;
+            const ry= resizeY;
+            setResizeX(0);
+            setResizeY(0);
+            props.onChange(produce(props.region, draft => {
+                draft.x2 = Math.round(draft.x2 + pxToPtH(rx));
+                draft.y2 = Math.round(draft.y2 + pxToPtV(ry));
+            }));
+        }
+    }, { preventDefault: true });
 
     return (
-        <div className="RegionBox" style={style} draggable="true" onDragStart={onMoveDragStart} onDragEnd={onMoveDragEnd}>
+        <div className="RegionBox" style={style} {...dragBind()}>
             {props.text}
-            <div className="ResizeHandle" draggable="true" onDragStart={onResizeDragStart} onDragEnd={onResizeDragEnd} onDrag={onResizeDrag}/>
+            <div className="ResizeHandle" {...resizeBind()} style={{ touchAction: 'none' }}/>
         </div>
     );
 }
