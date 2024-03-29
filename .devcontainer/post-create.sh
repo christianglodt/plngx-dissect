d25ffacfaa0b78ac04edc5f5fe3c1259a66a776e
Christian Glodt <chris@mind.lu>
2024-03-29 09:13:16 +0000

    add austin to new requirements-dev.txt, add profiling script

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -7,3 +7,4 @@ npm install --include dev plngx-dissect-frontend --prefix plngx-dissect-frontend
 
 pip install wheel
 pip install -r backend/requirements.txt
+pip install -r backend/requirements-dev.txt
---
+++ backend/profile.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import asyncio
+
+import ruamel.yaml
+
+from pattern import Pattern
+from matching import get_documents_matching_pattern
+from document import DocumentBase
+
+
+async def pattern_matching(pattern: Pattern) -> list[DocumentBase]:
+    res: list[DocumentBase] = []
+    async for doc in get_documents_matching_pattern(pattern):
+        res.append(doc)
+    return res
+
+
+PATTERN = '''
+page: 0
+name: SoloMed Invoice
+checks:
+- type: correspondent
+  name: SoloMedGmbH
+- type: document_type
+  name: Bill
+regions:
+- x: 62.0
+  y: 416.0
+  x2: 546.0
+  y2: 707.0
+  regex: Invoice amount € (?P<Amount>[\\d,]+)
+fields:
+- name: Amount
+  template: '{{ Amount | replace(",", ".") }}'
+
+'''
+
+if __name__ == '__main__':
+    pattern = Pattern.model_validate(ruamel.yaml.YAML().load(ruamel.yaml.StringIO(PATTERN)))
+    print(asyncio.run(pattern_matching(pattern)))
