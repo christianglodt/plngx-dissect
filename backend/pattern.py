e9c24fee11c9fb8e2bd40e815c3e8dab21fedeb8
Christian Glodt <chris@mind.lu>
2025-01-07 08:44:19 +0000

    handle last page correctly

--- backend/pattern.py
+++ backend/pattern.py
@@ -184,6 +184,9 @@ class Pattern(pydantic.BaseModel):
         return True
 
     async def get_match_result(self, doc: document.Document, page_nr: int, paperless_doc: PaperlessDocument, client: PaperlessClient) -> list[CheckResult|None]:
+        if page_nr == -1:
+            page_nr = len(doc.pages) - 1
+
         if self.page == -1 and page_nr != len(doc.pages) - 1:
             return [None] * len(self.checks)
         if self.page != -1 and page_nr != self.page:
