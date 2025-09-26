293389e31f29693e1b384a364477c5a08ff5436c
Christian Glodt <chris@mind.lu>
2025-09-26 14:23:10 +0200

    Switch to uv for package management, adapt Dockerfile and devcontainer. For reasons of simplicity, the virtualenv is now located in /app/backend/.venv.

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,6 +5,6 @@ set -e
 nvm install 22
 npm install --include dev plngx-dissect-frontend --prefix plngx-dissect-frontend
 
-pip install wheel
-pip install -r backend/requirements.txt
-pip install -r backend/requirements-dev.txt
+pushd backend
+uv sync
+popd
