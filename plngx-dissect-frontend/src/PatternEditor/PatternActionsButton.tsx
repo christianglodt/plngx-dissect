5d8c44d71c8ecdfc678466d2bf1298816ae9597b
christianglodt <chris@mind.lu>
2026-07-23 09:28:42 +0000

    Disable "Process" button if pattern is disabled.

--- plngx-dissect-frontend/src/PatternEditor/PatternActionsButton.tsx
+++ plngx-dissect-frontend/src/PatternEditor/PatternActionsButton.tsx
@@ -47,7 +47,7 @@ const PatternActionsButton = () => {
                         <ButtonGroup orientation="vertical" variant="contained">
                             <SaveAsButton name={pattern.name}/>
                             <RenameButton name={pattern.name}/>
-                            <ConfirmButton style={{ borderColor: '#666' }} variant="outlined" icon={<Sync/>} disabled={isSaving || isModified || pattern === null || document === null}  dialogTitle="Process Document?" dialogText="Process the current document with this pattern?" color="warning" onConfirmed={onProcessDocumentConfirmed}>Process</ConfirmButton>
+                            <ConfirmButton style={{ borderColor: '#666' }} variant="outlined" icon={<Sync/>} disabled={isSaving || isModified || pattern === null || document === null || pattern.enabled === false}  dialogTitle="Process Document?" dialogText="Process the current document with this pattern?" color="warning" onConfirmed={onProcessDocumentConfirmed}>Process</ConfirmButton>
                             <ConfirmButton style={{ borderColor: '#666' }} variant="outlined" icon={<DeleteForever/>} disabled={isSaving} dialogTitle="Delete Pattern?" dialogText="Are you sure to delete this pattern?" color="error" onConfirmed={onDeleteClicked}>Delete</ConfirmButton>                        
                         </ButtonGroup>
                     </ClickAwayListener>
